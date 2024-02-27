c6f5279eaef965e4a009bd4ca936f6309f3c62d4
Musfirah Lodhi <Musfirahlodhi4@gmail.com>
2024-02-27 12:12:54 +0500

    assignment 15

---
+++ app.js
@@ -0,0 +1,88 @@
+// // ------Chap 1----------
+// // Q1
+// var a=1, b=2, c=5;
+// // Q2
+// // *legal Variables
+// var num=3;
+// var firstName = "Musfirah";
+// var name1 = "Musfirah";
+// var $age= 19;
+// var birth_year= 2004;
+// // Illegal variables
+// var 10= "age";
+// var !age= 10
+// var first name = "Musfirah"
+// var my-hobby = "enjoying"
+// var 123abc =" illegal variable"
+
+// // Q3
+// document.write("<pre><h1>“Rules for naming JS variables”</h1>\nVariable names can only contain numers,$ and_. For example $my_1stVariable\nVariable names are case sensitive\nVariable names should not be JS keywords</pre>")
+
+// // Chapter 5
+// // Q1
+// var num1 = 10;
+// var num2 = 20;
+// var result = num1+num2, result2 = num1-num2, result3= num1*num2, result4 =num1%num2
+// result3
+// document.write("the sum of two numbers "+ num1 + "and" + num2 +" " + "is" +" " + result )
+// document.write("the sum of two numbers "+ num1 + "and" + num2 +" " + "is" +" " + result2 )
+// document.write("the sum of two numbers "+ num1 + "and" + num2 +" " + "is" +" " + result3 )
+// document.write("the sum of two numbers "+ num1 + "and" + num2 +" " + "is" +" " + result4 )
+// // Q3
+// var num
+// document.write("“Value after variable declaration is: Undefined”.<br>")
+// var num = 50
+// document.write("“Initial value:”" + num + "<br>")
+// ++num
+// document.write("“value after increment is:”" + num + "<br>")
+// num +=7
+// document.write("Value after adding 7 is: " + num + "<br>");
+// --num
+// document.write("“value after decrement is:”" + num + "<br>");
+// var remainder = num % 3;
+// document.write("Remainder after dividing by 3 is: " + remainder + "<br>");
+
+// // Q4
+// var cost= 600, total = cost*5;
+// document.write("The total cost to buy 5 tickets to a movie is " + total + "PKR")
+
+// // Q5
+// var num=9
+// document.write(
+//     num +"*"+ 1+ "="+ num*1 + "<br>" +
+//     num +"*"+ 2+ "="+ num*2 + "<br>" +
+//     num +"*"+ 3+ "="+ num*3 + "<br>" +
+//     num +"*"+ 4+ "="+ num*4 + "<br>" +
+//     num +"*"+ 5+ "="+ num*5 + "<br>" +
+//     num +"*"+ 6+ "="+ num*6 + "<br>" +
+//     num +"*"+ 7+ "="+ num*7 + "<br>" +
+//     num +"*"+ 8+ "="+ num*8 + "<br>" +
+//     num +"*"+ 9+ "="+ num*9 + "<br>" +
+//     num +"*"+ 10+ "="+ num*10  
+
+//     )
+
+// // Q6
+//  var celsiusT=25
+//  var ctoF= (25*9/5)+32
+//  document.write(celsiusT+"C is " + ctoF + " F <br>")
+//  var fahrenheit = 90
+//  var ftoC= (34-32)*5/9
+//  document.write( fahrenheit + " F is " +ftoC + "C")
+
+// // Q7
+// document.write("<h1>Shopping Cart</h1> <br>")
+// var p1 =650, p2=100, q1=3, q2=7, total= (p1*q1)+(p2*q2)
+// document.write("The price of Item 1 is " + p1 + "<br>" +
+//  "Quantity of item 1 is " + q1 + "<br>"+
+//   "price of item 2 is " + p2 + "<br>" +
+//    "The quantity of Item 2 is " + q2 + "<br>" +
+//    "Total cost of your order is " + total  );
+
+// // Q8
+// var totalmarks=100, obtained=89, percentage=(obtained/totalmarks)*100
+// document.write("<h1>Mark Sheet</h1> <br>"+
+// "Total Marks="+totalmarks+"<br>"+
+// "Obtained Marks=" +obtained +"<br>" +
+// "Perscentage=" + percentage +"%"
+// )
